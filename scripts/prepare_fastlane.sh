48f194c16cd4603b1acbac75cb77cac28c590cac
Ulrik Gammelby <ug@nabto.com>
2018-10-25 22:20:02 +0200

    add note about keychain unlock

--- scripts/prepare_fastlane.sh
+++ scripts/prepare_fastlane.sh
@@ -4,3 +4,11 @@ sudo fastlane add_plugin versioning
 
 # show help
 fastlane init
+
+echo ""
+echo "To store cert repo credentials locally (prompted first time you run fastlane), "
+echo "you may need to unlock your keychain: "
+echo "  security -v unlock-keychain -p <password> /Users/`whoami`/Library/Keychains/login.keychain"
+echo ""
+
+
